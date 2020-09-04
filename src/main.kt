e74b0ccc1783b7ee1c5a0968f64b4918bc912700
Vinicius Mussato <vmussato.vermont@vstsalelo.onmicrosoft.com>
2020-09-03 23:45:21 -0300

    loop test

--- src/main.kt
+++ src/main.kt
@@ -1,29 +1,29 @@
 fun main() {
     println("Hello World")
 
-    val titular = "Vinícius de Morais Mussato"
-    val numeroConta: Int = 1000
-    var saldo = 0.0
+    for (i in 1..5) {
 
-    saldo += -100
+        val titular = "Vinícius $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-    println("O número da conta é $numeroConta")
-    println("O saldo é $saldo")
-    println("O títular é $titular")
 
+
+        println("O número da conta é $numeroConta")
+        println("O saldo é $saldo")
+        println("O títular é $titular")
+
+
+    }
+
+    //testaCondicoes(saldo)
+
+}
+
+fun testaCondicoes(saldo: Double) {
     when {
         saldo > 0.0 -> println("Conta é positiva")
         saldo == 0.0 -> println("Conta é neutra")
         else -> println("Conta é negativa")
-    }
-}
-
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0.0) {
-        println("Conta é positiva")
-    } else if (saldo == 0.0) {
-        println("Conta é neutra")
-    } else {
-        println("Conta é negativa")
     }
 }
